c67384b44ce554d5a685968ae6ea13440e4b2c3b
Marcus Weseloh <marcus@weseloh.cc>
2019-09-22 23:16:44 +0200

    Simple crashdump handler to write sysinfo to sd-card
    Triggered by simultaneous keypress of D1, D2, D3, G1, G2

---
+++ mgurdy-lib/src/crashdump.c
@@ -0,0 +1,65 @@
+/**
+ * A simple crashdump "daemon" that listens to the main mgurdy keys input
+ * and triggers a mgsysinfo dump to /data/crashdump.html before issuing a 
+ * reboot. Listens to simultaneous key-presses of D1, D2, D3, G1, G2
+ */
+#include <stdio.h>
+#include <fcntl.h>
+#include <linux/input.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+#define NUMKEYS (5)
+
+void int_handler(){
+    exit(0);
+}
+
+void dump_and_restart()
+{
+    system("/bin/echo 'heartbeat' > /sys/class/leds/string1/trigger");
+    system("/bin/echo 'heartbeat' > /sys/class/leds/string2/trigger");
+    system("/bin/echo 'heartbeat' > /sys/class/leds/string3/trigger");
+    system("/usr/bin/wget -q http://localhost:9999/live -O /data/crashdump.html");
+    system("/bin/sync");
+    printf("rebooting in 2 seconds!\n");
+    sleep(2);
+    system("/sbin/reboot");
+}
+
+int main()
+{
+    char devname[] = "/dev/input/event2";
+    int device = open(devname, O_RDONLY);
+    unsigned int keystate = 0;
+    int keys[NUMKEYS] = {106, 109, 112, 124, 127};
+    int i;
+    struct input_event ev;
+
+    signal(SIGINT, int_handler);
+
+    while(1)
+    {
+        read(device, &ev, sizeof(ev));
+        if (ev.type != 1) continue;
+
+        for (i=0; i < NUMKEYS; i++) {
+            if (ev.code == keys[i])
+                break;
+        }
+
+        if (i < NUMKEYS) {
+            if (ev.value) {
+                keystate |= (1UL << i);
+            } else {
+                keystate &= ~(1UL << i);
+            }
+
+            if (keystate == 31) {
+                dump_and_restart();
+                break;
+            }
+        }
+    }
+}
