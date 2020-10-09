5fb74b46f9ad56ac7d09e09d5423166e02ef697a
Marcus Weseloh <marcus@weseloh.cc>
2020-10-09 11:18:07 +0200

    Code cleanup: remove some warnings, add more error messages

--- mgurdy-lib/src/crashdump.c
+++ mgurdy-lib/src/crashdump.c
@@ -9,8 +9,18 @@
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #define NUMKEYS (5)
+
+
+void call(const char *cmd)
+{
+    int err = system(cmd);
+    if (err) {
+        perror("Error after system");
+    }
+}
 
 void int_handler(){
     exit(0);
@@ -20,18 +30,18 @@ void dump_and_restart()
 {
     int err;
 
-    system("/bin/echo 'heartbeat' > /sys/class/leds/string1/trigger");
-    system("/bin/echo 'heartbeat' > /sys/class/leds/string2/trigger");
-    system("/bin/echo 'heartbeat' > /sys/class/leds/string3/trigger");
+    call("/bin/echo 'heartbeat' > /sys/class/leds/string1/trigger");
+    call("/bin/echo 'heartbeat' > /sys/class/leds/string2/trigger");
+    call("/bin/echo 'heartbeat' > /sys/class/leds/string3/trigger");
     err = system("/usr/bin/wget -q -T 10 http://localhost:9999/live -O /data/crashdump.html");
     if (err) {
         // if getting the sysinfo fails, at least try to secure the current syslog
-        system("cp /var/log/messages /data/crashdump.html");
+        call("cp /var/log/messages /data/crashdump.html");
     }
-    system("/bin/sync");
+    call("/bin/sync");
     printf("rebooting in 2 seconds!\n");
     sleep(2);
-    system("/sbin/reboot");
+    call("/sbin/reboot");
 }
 
 int main()
@@ -41,13 +51,20 @@ int main()
     unsigned int keystate = 0;
     int keys[NUMKEYS] = {106, 109, 112, 124, 127};
     int i;
+    int rd;
     struct input_event ev;
 
     signal(SIGINT, int_handler);
 
     while(1)
     {
-        read(device, &ev, sizeof(ev));
+        rd = read(device, &ev, sizeof(ev));
+        if (rd < 0) {
+            if (errno != EAGAIN) {
+                perror("Read error on input device");
+                break;
+            }
+        }
         if (ev.type != 1) continue;
 
         for (i=0; i < NUMKEYS; i++) {
